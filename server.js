85c0bcd8aeac80c98088cb9d178ab45e85b776d0
John Peden <johnmahlon@hey.com>
2021-02-13 21:56:18 -0600

    more updates

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.post('/', async (req, res) => {
 
 app.listen(port, () => {
     parseFile("PUBLIC.json");
-    console.log(`Listening on port ${3000}`);
+    console.log(`Listening on port ${port}`);
 });
 
 function parseFile(name) {
